079b0245418db4f2da4d0e6467d56ff37a1bde6a
DBlatcher <Dv.bltchr@gmail.com>
2020-03-01 12:12:39 +0000

    more presets,correct css ordering

--- src/presets/colors.js
+++ src/presets/colors.js
@@ -2,9 +2,17 @@ const allGreen = (face, faceIndex) =>{
     face.classList.add ('preset-e3d-green')
 }
 
+const allBlack = (face, faceIndex) =>{
+    face.classList.add ('preset-e3d-black')
+}
+
+const checkered = (face, faceIndex) =>{
+    face.classList.add (faceIndex%2 === 0 ? 'preset-e3d-black' : 'preset-e3d-white')
+}
+
 const numbered = (face, faceIndex) =>{
     face.classList.add ('preset-e3d-numbered')
     face.innerHTML = `<p>${faceIndex+1}</p>`
 }
 
-export {allGreen, numbered}
+export {allGreen, numbered, checkered,allBlack}
