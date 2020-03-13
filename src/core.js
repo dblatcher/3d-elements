4c1e62e38663267a2d09b07b17b09fb4d95b8154
DBlatcher <Dv.bltchr@gmail.com>
2020-03-13 20:00:28 +0000

    create core module

---
+++ src/core.js
@@ -0,0 +1,36 @@
+import Cube from './shapes/cube'
+import Pyramid from './shapes/pyramid'
+import Dodecahedron from './shapes/dodecahedron'
+import Cuboid from './shapes/cuboid'
+import Decagon from './shapes/decagon'
+import Hexagon from './shapes/hexagon'
+import Icosahedron from './shapes/icosahedron'
+import Octahedron from './shapes/Octahedron'
+import PentagonalTrapezohedron from './shapes/PentagonalTrapezohedron'
+import Tetrahedron from './shapes/Tetrahedron'
+
+const make = {
+    Cube, Pyramid, Dodecahedron, Cuboid,Decagon,
+    Hexagon, Icosahedron, Octahedron, PentagonalTrapezohedron,
+    Tetrahedron
+}
+
+function buildShapesInDom () {
+    const shapeNameList = Object.keys(make)
+    const targetElementsList = document.querySelectorAll('[e3d-shape]')
+    let targetElements = []
+    for (let i = 0; i<targetElementsList.length; i++){
+        targetElements.push(targetElementsList[i])
+    }
+    targetElements.forEach( target => {
+        if (target.wasBuiltByScript) {return false}
+        const shapeName = target.getAttribute('e3d-shape')
+        if (shapeNameList.includes(shapeName)) {
+            make[shapeName].fromDom(target)
+        } else {
+            console.warn (`${shapeName} is not a supported shape.`)
+        }
+    })
+}
+
+export {make, buildShapesInDom}
